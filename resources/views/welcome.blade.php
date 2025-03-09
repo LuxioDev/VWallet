0f73989e133a194303b752638136867f69b6180e
LuxioDev <fernandez.lucasg4@gmail.com>
2025-03-09 16:11:41 -0300

    Third Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,5 +21,6 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
        <h1>Hola Mundo so matti</h1>
+       <h1>Hola mati, ahora entrego el codigo</h1>
     </body>
 </html>
